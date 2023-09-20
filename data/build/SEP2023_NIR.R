9e4d1d4569da3d59cd6f41243e6367b51be46eb8
TobieSurette <tobie.surette@gmail.com>
2023-09-20 09:37:29 -0300

    Added data from Sept 2023 CCGS Perley study

---
+++ data/build/SEP2023_NIR.R
@@ -0,0 +1,64 @@
+
+files <- dir(path = "C:/Users/SuretteTJ/Desktop/github/snow-crab-measurement-project/data/NIR/raw", full.names = TRUE)
+
+res <- NULL
+for (j in 1:length(files)){
+   print(files[j])
+   x <- read.csv(files[j])
+   
+   names(x) <- gsub("[.]+", ".", names(x))
+   names(x) <- gsub("[_]+", ".", names(x))
+   
+   fvars <- names(x)[grep("^[xX][0-9]+", names(x))]
+   
+   print(setdiff(names(x), fvars))
+   
+   ix <- seq(2, nrow(x), by = 2)
+   
+   for (i in 1:length(ix)){
+      tmp <- data.frame(sample.name        = x$sample.name[ix[i]],
+                        sample.class       = x$sample.class[ix[i]],
+                        session.id         = x$session.id[ix[i]],
+                        session            = x$session[ix[i]],
+                        session.date.utc   = x$session.date.utc[ix[i]],
+                        scan               = x$scan[ix[i]],
+                        crab.number        = x$crab.number[ix[i]],
+                        scan.chela.merus  = ifelse(is.null(x$scan.chela.merus[ix[i]]), "", x$scan.chela.merus[ix[i]]),
+                        scanner.serial     = x$scanner.serial[ix[i]],
+                        scan.configuration = x$scan.configuration[ix[i]],
+                        scan.temperature   = x$scan.temperature[ix[i]],
+                        scan.humidity      = x$scan.humidity[ix[i]],
+                        scan.pga           = x$scan.pga[ix[i]],
+                        wavelength         = unlist(x[ix[i]-1, fvars, drop = TRUE]),
+                        intensity          = unlist(x[ix[i], fvars, drop = TRUE]))
+      
+      if (is.null(res)) res <- tmp else res <- rbind(res, tmp)
+   }
+}
+
+res$scan.chela.merus <- tolower(gsub(" ", "", res$scan.chela.merus))
+
+res$date <- unlist(lapply(strsplit(res$session.date.utc, " "), function(x) x[1]))
+res$time <- unlist(lapply(strsplit(res$session.date.utc, " "), function(x) x[2]))
+
+names(res) <- gsub("sample.class", "size.class", names(res))
+names(res) <- gsub('scan.chela.merus', 'body.part', names(res))
+names(res) <- gsub('intensity', 'value', names(res))
+
+# Organize variables:
+remove <- c('session.id', 'sample.name', 'session', 'scan.configuration', 'scanner.serial', 'session.date.utc', 'scan.pga')
+res  <- res[, setdiff(names(res), remove)]
+vars <- c("date", "time", "crab.number", "size.class", "scan")
+res  <- res[, c(vars, setdiff(names(res), vars))]
+rownames(res) <- NULL
+
+res$measure   <- "intensity"
+res$measure[res$intensity < 5] <- "other"
+
+# Remove ad-hoc experiment:
+res <- res[res$crab.number <= 70, ]
+
+# Write reformatted data to file:
+write.csv(res, row.names = FALSE, file = "C:/Users/SuretteTJ/Desktop/github/snow-crab-measurement-project/data/SEP2023_NIR.csv")
+
+
---
+++ data/photos/ReadMe.txt
@@ -0,0 +1,4 @@
+Photos from the September 2023 study aboard the CCGS Perley.
+File names are of the form: 'SEP2023_CrabXXX_view.jpg' where XXX is the crab identification number (see SEP2023.xlsx file) and 'view' is either 'dorsal' or 'ventral', depending on whether the crab was flipped on its back or not.
+
+Other photos in the directory are archival photos not from the September 2023 study.
