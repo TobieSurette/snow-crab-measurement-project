244e45474ed27709bbb2f5bc8d857b7b7870f91c
Tobie Surette <tobie.surette@gmail.com>
2024-10-01 12:38:41 -0300

    Update crab photos.R

--- studies/July 2024 - Snow crab survey/build/crab photos.R
+++ studies/July 2024 - Snow crab survey/build/crab photos.R
@@ -1,18 +1,23 @@
 # Organize and rename crab photos:
 
-file <- data.frame(name = dir(path = "W:/Crab/Offshore Crab Common/Fishing Year 2024/Trawl Survey/photos/crab photos",
+file <- data.frame(name = dir(path = "W:/Crab/Offshore Crab Common/Fishing Year 2024/Trawl Survey/photos/crab",
                    full.names = TRUE, include.dirs = FALSE))
+file$full <- file$name
 
 file$name <- gsub(" +[Cc]rab +#", " crab #", file$name)
 file$name <- gsub(" +", " ", file$name)
 file$name <- gsub("[#] +", "# ", file$name)
-file <- data.frame(name = file$name[grep(" crab ", file$name)])
+file <- file[grep(" crab ", file$name), ]
 
-file$full <- file$name
 file$name <- unlist(lapply(strsplit(file$name, "/"), function(x) x[length(x)]))
 
 
-file$tow.id      <- unlist(lapply(strsplit(file$name, " "), function(x) x[1]))
+file$tow.id <- unlist(lapply(strsplit(file$name, " "), function(x) x[1]))
+
+
+for (i in as.character(0:9)) file$name <- gsub(paste0(i, "000") , paste0(i, " 000"), file$name)
+file$name <- gsub("110010" , "110 010", file$name)
+
 file$crab.number <- as.numeric(unlist(lapply(strsplit(file$name, " "), function(x) x[4])))
 item        <- unlist(lapply(strsplit(file$name, " "), function(x) x[5]))
 item        <- gsub("[.]JPG", "", item)
@@ -35,14 +40,14 @@ for (i in 1:nrow(crabs)){
    
    # Dorsal view:
    to <- paste0(path, paste0("SCS", 2024, "_", file$tow.id[ix[1]], "_crab_",  file$crab.number[ix[1]], "_dorsal.jpg"))
-   file.copy(file$full[ix[1]], to)
+   if (!file.exists(to)) file.copy(file$full[ix[1]], to)
    
    # Ventral view:
    if (length(ix >= 2)){
       for (j in 2:length(ix)){
          to <- paste0(path, paste0("SCS", 2024, "_", file$tow.id[ix[j]], "_crab_",  file$crab.number[ix[j]], "_ventral"))
          if (length(ix) > 2) to <- paste0(to, "_", j-1, ".jpg") else to <- paste0(to, ".jpg")
-         file.copy(file$full[ix[j]], to)      
+         if (!file.exists(to))  file.copy(file$full[ix[j]], to)      
       }
    }
 }
